4e5b12ca3907b9332e3c92c5db6aa3ccaf6a2829
Feronoyka <feronoyka@gmail.com>
2025-04-28 18:35:33 +0500

    refactor then and catch using async/await (with the comments)

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -9,13 +9,33 @@ class RandomQuote {
     return new Quote(id, text, author);
   }
 
-  static getRandomQuoteViaAPI() {
+  /**
+   * 1) Each async function return promise
+   * 2) If on the "await" line of code is rejected, code in the same block below "await" is not executed
+   * 3) Promise returned by getRandomQuoteViaAPI function is always fulfilled
+   * 4) Result "fulfilled" (promise) will be each either Quote or undefined
+   * 5) Therfore there is no have to call getRandomQuoteViaAPI function in try/catch (./RandomQuotesApp)
+   */
+
+  static async getRandomQuoteViaAPI() {
     const url = 'https://quoteslate.vercel.app/api/quotes/random';
-    return fetch(url, { headers: { 'Content-Type': 'application/json' } })
-      .then((res) => res.json())
-      .then(({ id, quote: text, author }) => new Quote(id, text, author))
-      .catch((error) => console.error('Error:', error));
+    const option = { headers: { 'Content-Type': 'application/json' } };
+    try {
+      const res = await fetch(url, option);
+      const { id, quote: text, author } = await res.json();
+      return new Quote(id, text, author);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
+
+  // static getRandomQuoteViaAPI() {
+  //   const url = 'https://quoteslate.vercel.app/api/quotes/random';
+  //   return fetch(url, { headers: { 'Content-Type': 'application/json' } })
+  //     .then((res) => res.json())
+  //     .then(({ id, quote: text, author }) => new Quote(id, text, author))
+  //     .catch((error) => console.error('Error:', error));
+  // }
 }
 
 export default RandomQuote;
